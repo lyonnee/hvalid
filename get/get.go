66a81c98cf506fa100b0bdaeb220ccb3926ddbe0
lyonnee <lyon.nee@outlook.com>
2024-04-16 14:40:55 +0800

    feat: 定义get接口

---
+++ get/get.go
@@ -0,0 +1,27 @@
+package get
+
+import (
+	"errors"
+
+	"github.com/lyonnee/hval"
+)
+
+func Get[T any](input any, validators ...hval.Validator[T]) (T, error) {
+	return get[T](input, validators...)
+}
+
+func get[T any](input any, validators ...hval.Validator[T]) (T, error) {
+	var t T
+
+	if res, ok := input.(T); ok {
+		for _, v := range validators {
+			if err := v.Validat(res); err != nil {
+				return t, err
+			}
+		}
+
+		return res, nil
+	}
+
+	return t, errors.New("type not match")
+}
