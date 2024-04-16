0afcab54b0b97acbcf9328b43e261208f143c149
lyonnee <lyon.nee@outlook.com>
2024-04-16 20:59:27 +0800

    feat: []byte类型化的验证器模板

---
+++ bytes.go
@@ -0,0 +1,17 @@
+package hval
+
+import (
+	"bytes"
+	"errors"
+)
+
+func ContainsBytes(subslice []byte) ValidatorFunc[[]byte] {
+	return ValidatorFunc[[]byte](func(field []byte) error {
+		ok := bytes.Contains(field, subslice)
+		if !ok {
+			return errors.New("not contains sub byte slice")
+		}
+
+		return nil
+	})
+}
