be99b8fd92da65ddf42c1c89907bc6e9c9d9e332
lyonnee <lyon.nee@outlook.com>
2024-04-16 20:55:28 +0800

    feat: number类型的验证器模板生成
    feat: number类型的验证器模板生成

---
+++ number.go
@@ -0,0 +1,23 @@
+package hval
+
+import (
+	"fmt"
+)
+
+func Min[T int | int8 | int16 | int32 | int64 | uint | uint8 | uint16 | uint32 | uint64 | float32 | float64](min T) ValidatorFunc[T] {
+	return ValidatorFunc[T](func(num T) error {
+		if num < min {
+			return fmt.Errorf("The num is less than the minimum value, mininum value: %v, input num: %v", min, num)
+		}
+		return nil
+	})
+}
+
+func Max[T int | int8 | int16 | int32 | int64 | uint | uint8 | uint16 | uint32 | uint64 | float32 | float64](max T) ValidatorFunc[T] {
+	return ValidatorFunc[T](func(num T) error {
+		if num > max {
+			return fmt.Errorf("The num is greater than the maximum value, maximum value: %v, input num: %v", max, num)
+		}
+		return nil
+	})
+}
